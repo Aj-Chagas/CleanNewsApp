2fc555cef5fde0df13c1d93287e16be62d0d554d
Anderson Chagas <anderson.1997reis@gmail.com>
2021-04-15 18:51:46 -0300

    Initial Commit

---
+++ CleanNewsApp/CleanNewsApp.h
@@ -0,0 +1,18 @@
+//
+//  CleanNewsApp.h
+//  CleanNewsApp
+//
+//  Created by Anderson Chagas on 15/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CleanNewsApp.
+FOUNDATION_EXPORT double CleanNewsAppVersionNumber;
+
+//! Project version string for CleanNewsApp.
+FOUNDATION_EXPORT const unsigned char CleanNewsAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CleanNewsApp/PublicHeader.h>
+
+
