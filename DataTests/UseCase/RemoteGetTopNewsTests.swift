20507005110c386f7a5df530f0fd0e4cc3b47b18
Anderson Chagas <anderson.1997reis@gmail.com>
2021-04-15 21:58:55 -0300

    move boilerplate to method on extension

--- DataTests/UseCase/RemoteGetTopNewsTests.swift
+++ DataTests/UseCase/RemoteGetTopNewsTests.swift
@@ -19,49 +19,26 @@ class RemoteGetTopNewsTests: XCTestCase {
     }
 
     func test_getTopHeadlineNews_should_completes_with_error_if_client_complete_with_error() {
-        let url = URL(string: "any_url.com")!
-        let (sut, httpCLient) = makeSut(url: url)
-        let exp = expectation(description: "waiting")
-        sut.getTopHeadlineNews { result in
-            switch result {
-            case .success: XCTFail("Expected failure and received success instead")
-            case .failure(let error): XCTAssertEqual(error, DomainError.unexpected)
-            }
-            exp.fulfill()
-        }
-        httpCLient.completionWithError()
-        wait(for: [exp], timeout: 1)
+        let (sut, httpCLient) = makeSut()
+        expect(sut: sut, expectResult: .failure(.unexpected), when: {
+            httpCLient.completionWithError()
+        })
     }
 
     func test_getTopHeadlineNews_should_completes_with_error_if_client_complete_with_invalid_data() {
-        let url = URL(string: "any_url.com")!
-        let (sut, httpCLient) = makeSut(url: url)
-        let exp = expectation(description: "waiting")
-        sut.getTopHeadlineNews { result in
-            switch result {
-            case .success: XCTFail("Expected failure and received success instead")
-            case .failure(let error): XCTAssertEqual(error, DomainError.failWrapJson)
-            }
-            exp.fulfill()
-        }
-        httpCLient.completionWithInvalidData()
-        wait(for: [exp], timeout: 1)
+        let (sut, httpCLient) = makeSut()
+        expect(sut: sut, expectResult: .failure(.failWrapJson), when: {
+            httpCLient.completionWithInvalidData()
+        })
     }
     
     func test_getTopHeadlineNews_should_completes_with_success_if_client_complete_with_valid_data() {
         let url = URL(string: "any_url.com")!
         let (sut, httpCLient) = makeSut(url: url)
-        let expectedData: Data = makeNews().toData()!
-        let exp = expectation(description: "waiting")
-        sut.getTopHeadlineNews { result in
-            switch result {
-            case .success(let data): XCTAssertEqual(expectedData.toModel(), data)
-            case .failure: XCTFail("Expected success and received error instead")
-            }
-            exp.fulfill()
-        }
-        httpCLient.completionWithSuccess(with: expectedData)
-        wait(for: [exp], timeout: 1)
+        let expectedResult: News = makeNews()
+        expect(sut: sut, expectResult: .success(expectedResult), when: {
+            httpCLient.completionWithSuccess(with: expectedResult.toData()!)
+        })
     }
 
     func test_add_should_not_complete_if_sut_has_been_deallocated() {
@@ -85,6 +62,20 @@ extension RemoteGetTopNewsTests {
         checkMemoryLeak(for: sut)
         checkMemoryLeak(for: httpClientSpy)
         return (sut, httpClientSpy)
+    }
+    
+    func expect(sut: RemoteGetTopNews, expectResult: Result<News, DomainError>, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+        let exp = expectation(description: "waiting")
+        sut.getTopHeadlineNews { receivedResult in
+            switch (receivedResult, expectResult) {
+            case (.failure(let receivedError), .failure(let expectedError)): XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+            case (.success(let receivedNews), .success(let expectedNews)): XCTAssertEqual(receivedNews, expectedNews, file: file, line: line)
+            default: XCTFail("Expected \(expectResult) received \(receivedResult) instead", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        action()
+        wait(for: [exp], timeout: 1)
     }
     
     func makeNews() -> News {
