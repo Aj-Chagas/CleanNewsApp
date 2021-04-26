c68eaa5f65a37a3577505dfc8631298651bbf9bb
Anderson Chagas <anderson.1997reis@gmail.com>
2021-04-26 20:25:52 -0300

    add infra layer target in project

---
+++ InfraTests/InfraTests.swift
@@ -0,0 +1,16 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Anderson Chagas on 26/04/21.
+//
+
+import XCTest
+
+class InfraTests: XCTestCase {
+
+    func test() {
+        
+    }
+
+}
