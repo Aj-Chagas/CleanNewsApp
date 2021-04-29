a571034658e1d51d4e560b2c67281228e640e9bf
Anderson Chagas <anderson.1997reis@gmail.com>
2021-04-29 19:37:23 -0300

    testing all case URLSessionAdapter

--- InfraTests/InfraTests.swift
+++ InfraTests/InfraTests.swift
@@ -51,7 +51,7 @@ class InfraTests: XCTestCase {
         var request: URLRequest?
         URLProtocolStubs.observerRequest(completion: { request = $0 })
         sut.get(to: url, completion: { _ in exp.fulfill() })
-        wait(for: [exp], timeout: 5)
+        wait(for: [exp], timeout: 1)
         XCTAssertEqual(url, request?.url)
         XCTAssertEqual("GET", request?.httpMethod)
     }
@@ -59,7 +59,40 @@ class InfraTests: XCTestCase {
     func test_get_should_complete_with_error_when_received_error() {
         expectResult(.failure(.noConnectivy), when: (data: nil, response: nil, error: makeError()))
     }
+    
+    func test_get_should_complete_with_error_when_received_data_and_error() {
+        expectResult(.failure(.noConnectivy), when: (data: makeValidData(), response: nil, error: makeError()))
+    }
+    
+    func test_get_should_complete_with_error_when_not_received_data_and_error() {
+        expectResult(.failure(.serverError), when: (data: nil, response: nil, error: nil))
+    }
 
+    func test_get_should_complete_with_success_when_request_completes_with_data_and_status_code_200() {
+        expectResult(.success(makeValidData()), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 200), error: nil))
+        expectResult(.success(makeValidData()), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 299), error: nil))
+        expectResult(.success(makeValidData()), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 202), error: nil))
+        expectResult(.success(makeValidData()), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 203), error: nil))
+        expectResult(.success(makeValidData()), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 204), error: nil))
+    }
+    
+    func test_get_should_complete_with_success_when_request_completes_with_data_and_status_code_between_400_499() {
+        expectResult(.failure(.badRequest), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 400), error: nil))
+        expectResult(.failure(.badRequest), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 499), error: nil))
+        expectResult(.failure(.badRequest), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 450), error: nil))
+    }
+    
+    func test_get_should_complete_with_success_when_request_completes_with_data_and_status_code_between_500_599() {
+        expectResult(.failure(.serverError), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 500), error: nil))
+        expectResult(.failure(.serverError), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 599), error: nil))
+        expectResult(.failure(.serverError), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 550), error: nil))
+    }
+    
+    func test_get_should_complete_with_success_when_request_completes_with_data_status_code_non_between_200_to_599() {
+        expectResult(.failure(.noConnectivy), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 600), error: nil))
+        expectResult(.failure(.noConnectivy), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 100), error: nil))
+        expectResult(.failure(.noConnectivy), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 800), error: nil))
+    }
 }
 
 extension InfraTests {
@@ -87,15 +120,23 @@ extension InfraTests {
             
             exp.fulfill()
         }
-        wait(for: [exp], timeout: 5)
+        wait(for: [exp], timeout: 1)
+    }
+
+    func makeValidData() -> Data {
+        Data("{\"name\": \"Anderson\"}".utf8)
     }
 
     func makeError() -> NSError {
         NSError(domain: "any_error", code: 0)
     }
-    
+
     func makeUrl() -> URL {
         URL(string: "http://any-url.com")!
+    }
+
+    func makeHttpResponse(statusCode: Int = 200) -> HTTPURLResponse {
+        HTTPURLResponse(url: makeUrl(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
     }
 }
 
