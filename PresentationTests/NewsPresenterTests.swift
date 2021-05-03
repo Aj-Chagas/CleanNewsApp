f152da0b128df3e76616967b10fec68c7cad6762
Anderson Chagas <anderson.1997reis@gmail.com>
2021-05-03 16:18:23 -0300

    presentation target created

---
+++ PresentationTests/NewsPresenterTests.swift
@@ -0,0 +1,16 @@
+//
+//  PresentationTests.swift
+//  PresentationTests
+//
+//  Created by Anderson Chagas on 03/05/21.
+//
+
+import XCTest
+
+class NewsPresenterTests: XCTestCase {
+
+    func test_() {
+        
+    }
+
+}
