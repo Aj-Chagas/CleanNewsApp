cd4ca62721b81d0f50b6896a7652451560fbf56c
Anderson Chagas <anderson.1997reis@gmail.com>
2021-05-03 16:58:43 -0300

    ensure that call fetchNews and show loading in display

--- PresentationTests/NewsPresenterTests.swift
+++ PresentationTests/NewsPresenterTests.swift
@@ -7,19 +7,108 @@
 
 import XCTest
 import Domain
+import Data
+
+public protocol LoadingView {
+    func display(viewModel: LoadingViewModel)
+}
+
+public struct LoadingViewModel: Equatable {
+    public let isLoading: Bool
+    
+    public init(isLoading: Bool) {
+        self.isLoading = isLoading
+    }
+}
+
+public protocol NewsDelegate {
+    func didFetch(news: News?)
+}
 
 public final class NewsPresenter {
     
+    private let loadingView: LoadingView
+    private let fetchTopHealineNews: FetchTopHeadlineNews
+    private let delegate: NewsDelegate
+    
+    init(loadingView: LoadingView,
+         fetchTopHealineNews: FetchTopHeadlineNews,
+         delegate: NewsDelegate) {
+        self.loadingView = loadingView
+        self.fetchTopHealineNews = fetchTopHealineNews
+        self.delegate = delegate
+    }
+    
     public func fetchNews() {
-        
+        loadingView.display(viewModel: LoadingViewModel(isLoading: true))
+        fetchTopHealineNews.fetchTopHeadlineNews { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let news): self.delegate.didFetch(news: news)
+            case .failure: self.delegate.didFetch(news: nil)
+            }
+        }
     }
     
 }
 
 class NewsPresenterTests: XCTestCase {
 
-    func test_() {
+    func test_fetchNews_should_show_loading_when_call_webService() {
+        let loadingView = LoadingViewSpy()
+        let fetchTopHealineNews = FetchTopHeadlineNewsSpy()
+        let delegate = NewsDelegateSpy()
+        let sut = NewsPresenter(loadingView: loadingView,
+                                 fetchTopHealineNews: fetchTopHealineNews,
+                                 delegate: delegate)
+        
+        sut.fetchNews()
+        
+        XCTAssertTrue((loadingView.isLoadingSpy))
+        checkMemoryLeak(for: sut)
         
     }
 
+}
+
+extension NewsPresenterTests {
+    
+    class NewsDelegateSpy: NewsDelegate {
+        
+        var news: News?
+        
+        func didFetch(news: News?) {
+            self.news = news
+        }
+        
+    }
+    
+    class LoadingViewSpy: LoadingView {
+        
+        var isLoadingSpy: Bool = false
+        
+        public init() {}
+        
+        func display(viewModel: LoadingViewModel) {
+            isLoadingSpy = viewModel.isLoading
+        }
+    }
+    
+    class FetchTopHeadlineNewsSpy: FetchTopHeadlineNews {
+        
+        var completion: ((Result<News, DomainError>) -> Void)?
+
+        func fetchTopHeadlineNews(completion: @escaping (Result<News, DomainError>) -> Void) {
+            self.completion = completion
+        }
+        
+        func completionWithError() {
+            completion?(.failure(.unexpected))
+        }
+        
+        func completionWithNews(news: News) {
+            completion?(.success(news))
+        }
+        
+    }
 }
