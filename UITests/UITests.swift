8dba000c5c38c2d0d7f68c8fd5e18e424d99a339
Anderson Chagas <anderson.1997reis@gmail.com>
2021-05-04 19:05:10 -0300

    add new UI target

---
+++ UITests/UITests.swift
@@ -0,0 +1,16 @@
+//
+//  UITests.swift
+//  UITests
+//
+//  Created by Anderson Chagas on 04/05/21.
+//
+
+import XCTest
+@testable import UI
+
+class HomeViewControllerTests: XCTestCase {
+
+    func test_() {
+    }
+
+}
