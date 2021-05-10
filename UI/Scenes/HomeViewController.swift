3860d9d3c0a706ce34600c5fc250dbdb8345db13
Anderson Chagas <anderson.1997reis@gmail.com>
2021-05-10 19:24:12 -0300

    installing quick, nimble and snapshot pods

--- UI/Scenes/HomeViewController.swift
+++ UI/Scenes/HomeViewController.swift
@@ -9,15 +9,26 @@ import UIKit
 import Presentation
 import Domain
 
+class HomeView: UIView {
+    
+    override init(frame: CGRect = .zero) {
+        super.init(frame: frame)
+        backgroundColor = .darkGray
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 class HomeViewController: UIViewController {
     
     var fetchNews: (() -> Void)?
+    
+    private var mainView = HomeView()
 
     override func loadView() {
-        super.loadView()
-        let view = UIView(frame: UIScreen.main.bounds)
-        view.backgroundColor = .darkGray
-        self.view = view
+        self.view = mainView
     }
     
     override func viewDidLoad() {
---
+++ UITests/Snapshot/HomeViewSnapshotTest.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewSnapshotTest.swift
+//  UITests
+//
+//  Created by Anderson Chagas on 05/05/21.
+//
+
+import Quick
+import Nimble
+import Nimble_Snapshots
+import UIKit
+@testable import UI
+
+class MySpec: QuickSpec {
+    override func spec() {
+        
+        it("has valid snapshot") {
+            let frame = CGRect(x: 0, y: 0, width: 375, height: 620)
+            let view = HomeView(frame: frame)
+            expect(view) == recordSnapshot()
+        }
+        
+    }
+}
