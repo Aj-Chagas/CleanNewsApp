a95e712f665537588c04e5e6f148f4e07e10e7d4
Anderson Chagas <anderson.1997reis@gmail.com>
2021-05-14 20:47:51 -0300

    add new target networkImage

---
+++ NetworkImageTests/NetworkImageTests.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkImageTests.swift
+//  NetworkImageTests
+//
+//  Created by Anderson Chagas on 14/05/21.
+//
+
+import XCTest
+@testable import NetworkImage
+
+class NetworkImageTests: XCTestCase {
+
+    func test_() {
+        
+    }
+}
