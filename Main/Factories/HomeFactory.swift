8ad4fbd12dca9d2d512ccba2a1644a3da4cd498f
Anderson Chagas <anderson.1997reis@gmail.com>
2021-05-15 20:12:45 -0300

    update url string

--- Main/Factories/HomeFactory.swift
+++ Main/Factories/HomeFactory.swift
@@ -15,7 +15,7 @@ class HomeFactory {
     static func makeViewController() -> HomeViewController {
         let controller = HomeViewController(mainView: HomeView())
         let client = URLSessionAdapter()
-        let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=288e2e5064f4453bb4686e65de93e1e3")!
+        let url = URL(string: "https://newsapi.org/v2/top-headlines?country=pt&apiKey=288e2e5064f4453bb4686e65de93e1e3")!
         let remoteNews = RemoteFetchTopNews(httpGetClient: client, url: url)
         let presenter = NewsPresenter(loadingView: controller, fetchTopHealineNews: remoteNews, delegate: controller)
         
