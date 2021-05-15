cf014979711aa162ed72b4f57b1fd5d7f3292fd6
Anderson Chagas <anderson.1997reis@gmail.com>
2021-05-15 14:58:37 -0300

    implements UIColor extension

--- UI/Helpers/extensions/UIColorExtension.swift
+++ UI/Helpers/extensions/UIColorExtension.swift
@@ -6,7 +6,30 @@
 //
 
 import UIKit
-
+ 
 extension UIColor {
+    
+    convenience init(red: Int, green: Int, blue: Int, alpha: CGFloat = 1.0) {
+        
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(
+            red: CGFloat(red) / 255.0,
+            green: CGFloat(green) / 255.0,
+            blue: CGFloat(blue) / 255.0,
+            alpha: alpha
+        )
+    }
+
+    convenience init(hexdecimal: Int, alpha: CGFloat = 1.0) {
+        self.init(
+            red: (hexdecimal >> 16) & 0xFF,
+            green: (hexdecimal >> 8) & 0xFF,
+            blue: hexdecimal & 0xFF,
+            alpha: alpha
+        )
+    }
 
 }
